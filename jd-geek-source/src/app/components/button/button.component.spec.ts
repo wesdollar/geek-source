a290db784a0b825b22cbcf73de898a7ec9ad23f8
Jim Davis <jim.davis@ruralsource.com>
2020-10-15 20:30:29 -0400

    Lint changes

--- jd-geek-source/src/app/components/button/button.component.spec.ts
+++ jd-geek-source/src/app/components/button/button.component.spec.ts
@@ -1,16 +1,17 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { waitForAsync, ComponentFixture, TestBed } from "@angular/core/testing";
 import { ButtonComponent } from "./button.component";
 
 describe("ButtonComponent", () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ButtonComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ButtonComponent],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ButtonComponent);
