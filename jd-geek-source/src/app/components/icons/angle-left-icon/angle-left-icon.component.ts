8455c667c490e960be0376157db0294ed0f10268
jamesmdavisjr <jamesmdavisjr.se@gmail.com>
2021-01-14 09:47:47 -0500

    Added new icon components as well refactoring exsisting icon component to have a size input parameter so that the size can be adjusted for specific use

--- jd-geek-source/src/app/components/icons/angle-left-icon/angle-left-icon.component.ts
+++ jd-geek-source/src/app/components/icons/angle-left-icon/angle-left-icon.component.ts
@@ -1,11 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { SizeProp } from "@fortawesome/fontawesome-svg-core";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-angle-left-icon",
-  template: "<fa-icon [icon]='faAngleLeft'></fa-icon>",
+  template: "<fa-icon [size]='iconSize' [icon]='faAngleLeft'></fa-icon>",
   styleUrls: ["./angle-left-icon.component.scss"],
 })
 export class AngleLeftIconComponent {
+  @Input() iconSize: SizeProp;
   faAngleLeft = faAngleLeft;
 }
