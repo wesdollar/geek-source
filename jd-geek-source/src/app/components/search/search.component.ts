3f95ad3d281d8e6d545b515fed552bb6711113c6
jamesmdavisjr <jamesmdavisjr.se@gmail.com>
2021-03-22 12:17:54 -0400

    Refactored to use css variables and component variables to set colors.

--- jd-geek-source/src/app/components/search/search.component.ts
+++ jd-geek-source/src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostBinding, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-search",
@@ -6,7 +6,18 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./search.component.scss"],
 })
 export class SearchComponent implements OnInit {
+  @Input() bottomBorderColor: string;
+  @HostBinding("style.--box-bottom-line") private boxBottomLineColor: string;
+  @Input() backgroundColor: string;
+  @HostBinding("style.--box-background") private boxBackgroundColor: string;
+  @Input() textColor: string;
+  @HostBinding("style.--box-text") private boxTextColor: string;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.boxBottomLineColor = this.bottomBorderColor;
+    this.boxBackgroundColor = this.backgroundColor;
+    this.boxTextColor = this.textColor;
+  }
 }
