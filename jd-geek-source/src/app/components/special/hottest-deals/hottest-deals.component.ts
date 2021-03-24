5220de8113e67a522ad12d8c8a4b8d33ab9ea3c0
jamesmdavisjr <jamesmdavisjr.se@gmail.com>
2021-03-24 16:57:48 -0400

    Minor change for push

--- jd-geek-source/src/app/components/special/hottest-deals/hottest-deals.component.ts
+++ jd-geek-source/src/app/components/special/hottest-deals/hottest-deals.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-hottest-deals",
   templateUrl: "./hottest-deals.component.html",
   styleUrls: ["./hottest-deals.component.scss"],
 })
-export class HottestDealsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class HottestDealsComponent {}
